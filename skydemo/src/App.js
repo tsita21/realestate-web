fd6611edf2160c4feba2e88f1a6a722e6832ed32
tsita21 <natanemlov@gmail.com>
2023-10-11 08:13:18 -0900

    initial commit

---
+++ skydemo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import contactus from './components/contactus.js';
+
+function App() {
+  return (
+    <div>
+      <cuntactus/>
+    </div>
+  );
+}
+
+export default App;
---
+++ skydemo/src/components/contactus.js
@@ -0,0 +1,8 @@
+export default function contactus(){
+  return(
+    <div>
+      <h1>hello</h1>
+    </div>
+
+  )
+}
