05f79be0cbab90f9cc89e766d99809fb4aa85c7e
tsita21 <natanemlov@gmail.com>
2023-10-12 09:38:14 -0900

    tsita group commit

--- skydemo/src/App.js
+++ skydemo/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import Contactus from './components/contactus.js';
+import {Contactus} from './components/contactus';
+
 
 function App() {
   return (
--- skydemo/src/components/contactus.js
+++ skydemo/src/components/contactus.js
@@ -1,29 +1,82 @@
-export default function Contactus(){
+import React, {useState} from "react";
+import call_icon from '../components/image/call.png';
+import envelope_icon from '../components/image/envelope.png';
+import iphone_icon from '../components/image/iphone.png'; 
+import facebook_icon from '../components/image/facebook.png';
+import youtube_icon from '../components/image/youtube.png';
+
+export const Contactus=()=>{
+  const [email,setEmail] = useState('');
+  const [name,setName] = useState('');
+  const [message,setMessage] =useState('');
+
+  const handlesubmit =(e)=>{
+    e.preventDefault();
+    console.log(email);
+    console.log(name);
+    console.log(message);
+  }
+
   return(
     <form>
       <div>
-      <h1>CONTACT US</h1>
-      <h5>CONTACT US</h5>
-      <p>Get In Touch</p>
+        <div className="top">
+          <img src={"./image/img.png"} alt=""/>
+          <h1 >Contact Us </h1>
+        </div>
+      <h3>Contact Us</h3>
+      <h1 className="get">Get In Touch</h1>
     </div>
 
     <div className="container">
-      <div>
+      <div className="content">
         <h2>Send us a message</h2>
-        <input type="name" name="name" id="name" placeholder="Name" required/>
-        <input type="email" name="email" id="" placeholder="Email"/>
-        <input type="message" name="message" id="" placeholder="Message"/>
+        <input onChange={e=>setName(e.target.value)} type="name" name="name" id="name" placeholder="Name" required/>
+        <input onChange={e=>setEmail(e.target.value)} type="email" name="email" id="" placeholder="Email" required/>
+        <input onChange={e=>setMessage(e.target.value)} className="massage" type="message" name="message" id="" placeholder="Message" required/>
         <button>Send</button>
       </div>
-      <div>
+      <div className="content1">
         <h2>Contact info</h2>
-        <a href="https://www.flaticon.com/free-icons/phone" title="phone icons">+251909424567</a>
-        <a href="https://www.flaticon.com/free-icons/phone" title="phone icons">+251909424567</a>
-        <a href="https://www.flaticon.com/free-icons/phone" title="phone icons">+251909424567</a>
-        <a href="https://www.flaticon.com/free-icons/phone" title="phone icons">+251909424567</a>
+       <div className="img-container">
+        <div><img src={call_icon}/></div>
+        <div><p className='p-tag'>+251909424567</p></div>
+        
+       </div>
+       <div className="img-container">
+        <div><img src={call_icon}/></div>
+        <div><p className='p-tag'>+251909424567</p></div>        
+       </div>
+       <div className="img-container">
+        <div><img src={iphone_icon}/></div>
+        <div><p className='p-tag'>+251909424567</p></div>        
+       </div>
+       <div className="img-container">
+        <div><img src={iphone_icon}/></div>
+        <div><p className='p-tag'>+251909424567</p></div>        
+       </div>
+       <div className="img-container">
+       <div><img src={envelope_icon}/></div>
+        <div><p className='p-tag'>SkyRealEstate@gmail.com</p></div>        
+       </div>
+       <div className="img-container">
+       <div><p className='p-tag1'>Social Media</p></div>
+        <div><img className="unique" src={facebook_icon}/></div>
+        <div><img className="unique" src={youtube_icon}/></div>  
+       </div>
       </div>
 
     </div>
+    <div className="container1">
+      <div className="end">
+      <p >copy right @ Sky real estate</p>
+      </div>
+      <div className="end1">
+      <p >Powered by sky real estate </p>
+
+      </div>
+    </div>
+    
     </form>
     
 
